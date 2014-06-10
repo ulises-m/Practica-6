b640a96bcce41247214d690dde88e1df3ee8f473
ulises-m <ulises_cruz_19@outlook.com>
2014-06-10 14:27:13 -0500

    Initial commit

---
+++ Formulario.php
@@ -0,0 +1,37 @@
+<html>
+ <body>
+  <form method="POST" action="Guardar.php">
+   
+
+      <table border="1">
+      <tr>
+        <td><strong>       </strong></td>
+        <td><strong>Poducto</strong></td>
+        <td><strong>Precio</strong></td>
+      </tr>
+       
+      <tr>
+        <td>1</td>
+        <td><input type="text" size="25" value="" name="producto1"></td>
+        <td><input type="text" size="25" value="" name="precio1"></td>
+      </tr>
+       
+      <tr>
+        <td>2</td>
+        <td><input type="text" size="25" value="" name="producto2"></td>
+        <td><input type="text" size="25" value="" name="precio2"></td>
+      </tr>
+       
+      <tr>
+        <td>3</td>
+        <td><input type="text" size="25" value="" name="producto3"></td>
+        <td><input type="text" size="25" value="" name="precio3"></td>
+      </tr>
+      </table>
+
+
+
+   <input type="submit" value="Enviar" ><br>
+  </form>
+ </body>
+</html>
